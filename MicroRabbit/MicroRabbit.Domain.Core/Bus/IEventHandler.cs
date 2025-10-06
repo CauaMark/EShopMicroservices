5e28b41fed84b113e62e3361de32a3721e6d98ba
Cauã Mark de Pinho <caua.pinho@gmail.com>
2025-10-06 19:31:07 -0300

    Create Domain Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    internal interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handler(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
